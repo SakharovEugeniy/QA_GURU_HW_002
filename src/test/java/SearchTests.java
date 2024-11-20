683086373c76cb9719f9f8a9e2bfeae4fe169cf5
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-20 17:45:28 +0300

    add first test

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -5,4 +5,12 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+
+    }
 }
