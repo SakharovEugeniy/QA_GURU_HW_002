1908b2c95cc955656ed817f30738506e3fb17163
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-20 17:40:19 +0300

    create SearchTests

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,8 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+}
