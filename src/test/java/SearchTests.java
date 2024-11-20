3fe74b4e8c0da9a813af0e61493a31da1de54379
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-20 17:51:34 +0300

    add configuration

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -5,4 +5,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+        Configuration.pageLoadStrategy = "eager";
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+
+    }
 }
